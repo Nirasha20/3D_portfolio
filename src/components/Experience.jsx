86d409ba63a1abcafcc7f194188070e454ec6cb9
Nirasha <demelwgnd.22@uom.lk>
2025-10-18 02:15:51 +0530

    Feat: correct all

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -71,11 +71,10 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
-          What I have done so far
-        </p>
+        
+        
         <h2 className={`${styles.sectionHeadText} text-center`}>
-          Projects
+          Education
         </h2>
       </motion.div>
 
